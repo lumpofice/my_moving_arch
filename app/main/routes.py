8ec3aa73eef90dd5b5fb53da2aa644e117f155bd
Jonathan Parker <jparker007@gmail.com>
2023-07-01 07:05:06 -0400

    adding the template and stylesheet and route and files for the land_of_milk_and_honey viewpage

--- app/main/routes.py
+++ app/main/routes.py
@@ -12,6 +12,8 @@ def visual_art():
     if request.method == 'POST':
         if request.form['submit_button'] == 'Robots Grow Out of Keyboards':
             return redirect(url_for('main.robots_grow_out_of_keyboards'))
+        if request.form['submit_button'] == 'Land of Milk and Honey':
+            return redirect(url_for('main.land_of_milk_and_honey'))
     return render_template('visual_art.html', title='My Moving Arch')
 
 
@@ -19,4 +21,11 @@ def visual_art():
         methods=['GET', 'POST'])
 def robots_grow_out_of_keyboards():
     return render_template('robots_grow_out_of_keyboards.html', \
+            title='My Moving Arch')
+
+
+@bp.route('/visual_art/land_of_milk_and_honey', \
+        methods=['GET', 'POST'])
+def land_of_milk_and_honey():
+    return render_template('land_of_milk_and_honey.html', \
             title='My Moving Arch')
