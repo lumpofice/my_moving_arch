d540158f01f7362ca449e81648ee5fb3aa2c0523
Jonathan Parker <jparker007@gmail.com>
2023-07-27 20:27:35 -0400

    adding more visuals to the metric topology view

--- app/static/js/metric_topology.js
+++ app/static/js/metric_topology.js
@@ -1,5 +1,221 @@
+// One-dimensional real line filled with interval of a specified distance ----
 // ---------------------------------------------------------------------------
-// radius 15 nodes
+// 4 intervals ---------------------------------------------------------------
+var w_svg_for_line = 960;
+var h_svg_for_line = 150;
+
+var svg_for_line_0 = d3.select("#math0")
+  .append("svg")
+    .attr("viewBox", "0 0 " + w_svg_for_line + " " + h_svg_for_line);
+
+svg_for_line_0.append("line")
+    .style("stroke", "#759e16")
+    .style("stroke-width", 5)
+    .attr("x1", 0)
+    .attr("y1", h_svg_for_line/2)
+    .attr("x2", w_svg_for_line)
+    .attr("y2", h_svg_for_line/2);
+
+var parenthesis_open_0 = d3.path();
+var parenthesis_close_0 = d3.path();
+
+// Distance of right-most pixel of focus of parenthesis to i is 18px 
+// when convexity is 40
+var convexity = 40;
+
+var drawn_convexity = 18;
+var vertical_distance = 90;
+
+// The left-most pixel of the focus of the parenthesis to i is 23px 
+// when parenthesis_width is 5
+var parenthesis_width = 5;
+
+// Given n is "number of intervals", increment is 
+// (960 - [ n*( (2*parenthesis_width) + (2*drawn_convexity) ) ] )/n
+var increment_0 = 194;
+
+var distance_0 = increment_0 + (2*drawn_convexity) + (2*parenthesis_width);  
+
+for (let i = 23; i < 937; i = i + distance_0) {
+	parenthesis_open_0.moveTo(
+		i, 
+		(h_svg_for_line/2) + (vertical_distance/2)
+	);
+	parenthesis_open_0.quadraticCurveTo(
+		i - convexity, 
+		h_svg_for_line/2, 
+		i, 
+		(h_svg_for_line/2) - (vertical_distance/2)
+	);
+	
+	svg_for_line_0.append("path")
+	    .style("stroke", "#9977bd")
+	    .style("stroke-width", parenthesis_width)
+	    .attr("d", parenthesis_open_0)
+	    .attr("fill", "none");
+	
+	parenthesis_close_0.moveTo(
+		i + increment_0, 
+		(h_svg_for_line/2) + (vertical_distance/2)
+	);
+	parenthesis_close_0.quadraticCurveTo(
+		i + increment_0 + convexity, 
+		h_svg_for_line/2,
+		i + increment_0,
+		(h_svg_for_line/2) - (vertical_distance/2)
+	);
+
+	svg_for_line_0.append("path")
+	    .style("stroke", "#9977bd")
+	    .style("stroke-width", parenthesis_width)
+	    .attr("d", parenthesis_close_0)
+	    .attr("fill", "none");
+}
+
+// ---------------------------------------------------------------------------
+// 8 intervals ---------------------------------------------------------------
+var svg_for_line_1 = d3.select("#math1")
+  .append("svg")
+    .attr("viewBox", "0 0 " + w_svg_for_line + " " + h_svg_for_line);
+
+svg_for_line_1.append("line")
+    .style("stroke", "#759e16")
+    .style("stroke-width", 5)
+    .attr("x1", 0)
+    .attr("y1", h_svg_for_line/2)
+    .attr("x2", w_svg_for_line)
+    .attr("y2", h_svg_for_line/2);
+
+var parenthesis_open_1 = d3.path();
+var parenthesis_close_1 = d3.path();
+
+// Distance of right-most pixel of focus of parenthesis to i is 18px 
+// when convexity is 40
+
+// The left-most pixel of the focus of the parenthesis to i is 23px 
+// when parenthesis_width is 5
+
+// Given n is "number of intervals", increment is 
+// (960 - [ n*( (2*parenthesis_width) + (2*drawn_convexity) ) ] )/n
+var increment_1 = 74;
+
+var distance_1 = increment_1 + (2*drawn_convexity) + (2*parenthesis_width);  
+
+for (let i = 23; i < 937; i = i + distance_1) {
+	parenthesis_open_1.moveTo(
+		i, 
+		(h_svg_for_line/2) + (vertical_distance/2)
+	);
+	parenthesis_open_1.quadraticCurveTo(
+		i - convexity, 
+		h_svg_for_line/2, 
+		i, 
+		(h_svg_for_line/2) - (vertical_distance/2)
+	);
+	
+	svg_for_line_1.append("path")
+	    .style("stroke", "#9977bd")
+	    .style("stroke-width", parenthesis_width)
+	    .attr("d", parenthesis_open_1)
+	    .attr("fill", "none");
+
+	parenthesis_close_1.moveTo(
+		i + increment_1, 
+		(h_svg_for_line/2) + (vertical_distance/2)
+	);
+	parenthesis_close_1.quadraticCurveTo(
+		i + increment_1 + convexity, 
+		h_svg_for_line/2,
+		i + increment_1,
+		(h_svg_for_line/2) - (vertical_distance/2)
+	);
+
+	svg_for_line_1.append("path")
+	    .style("stroke", "#9977bd")
+	    .style("stroke-width", parenthesis_width)
+	    .attr("d", parenthesis_close_1)
+	    .attr("fill", "none");
+	
+}
+
+
+// ---------------------------------------------------------------------------
+// 16 intervals ---------------------------------------------------------------
+var svg_for_line_2 = d3.select("#math2")
+  .append("svg")
+    .attr("viewBox", "0 0 " + w_svg_for_line + " " + h_svg_for_line);
+
+svg_for_line_2.append("line")
+    .style("stroke", "#759e16")
+    .style("stroke-width", 5)
+    .attr("x1", 0)
+    .attr("y1", h_svg_for_line/2)
+    .attr("x2", w_svg_for_line)
+    .attr("y2", h_svg_for_line/2);
+
+var parenthesis_open_2 = d3.path();
+var parenthesis_close_2 = d3.path();
+
+// Distance of right-most pixel of focus of parenthesis to i is 4px 
+// when convexity is 10
+var convexity = 10;
+
+var drawn_convexity = 4;
+var vertical_distance = 50;
+
+// The left-most pixel of the focus of the parenthesis to i is 7px 
+// when parenthesis_width is 3
+var parenthesis_width = 3;
+
+// Given n is "number of intervals", increment is 
+// (960 - [ n*( (2*parenthesis_width) + (2*drawn_convexity) ) ] )/n
+var increment_2 = 46;
+
+var distance_2 = increment_2 + (2*drawn_convexity) + (2*parenthesis_width);  
+
+for (let i = 7; i < 953; i = i + distance_2) {
+	parenthesis_open_2.moveTo(
+		i, 
+		(h_svg_for_line/2) + (vertical_distance/2)
+	);
+	parenthesis_open_2.quadraticCurveTo(
+		i - convexity, 
+		h_svg_for_line/2, 
+		i, 
+		(h_svg_for_line/2) - (vertical_distance/2)
+	);
+	
+	svg_for_line_2.append("path")
+	    .style("stroke", "#9977bd")
+	    .style("stroke-width", parenthesis_width)
+	    .attr("d", parenthesis_open_2)
+	    .attr("fill", "none");
+	
+	parenthesis_close_2.moveTo(
+		i + increment_2, 
+		(h_svg_for_line/2) + (vertical_distance/2)
+	);
+	parenthesis_close_2.quadraticCurveTo(
+		i + increment_2 + convexity, 
+		h_svg_for_line/2,
+		i + increment_2,
+		(h_svg_for_line/2) - (vertical_distance/2)
+	);
+
+	svg_for_line_2.append("path")
+	    .style("stroke", "#9977bd")
+	    .style("stroke-width", parenthesis_width)
+	    .attr("d", parenthesis_close_2)
+	    .attr("fill", "none");
+}
+
+
+
+
+
+// Two-dimensional Euclidean space filled with balls of radius r -------------
+// ---------------------------------------------------------------------------
+// radius r=15 nodes
 var w_0 = 960;
 var h_0 = 300;
 
@@ -37,7 +253,7 @@ the_simulation_0.on("tick", function() {
 
 
 // ---------------------------------------------------------------------------
-// radius 10 nodes
+// radius r=10 nodes
 var w_1 = 960;
 var h_1 = 300;
 
@@ -75,7 +291,7 @@ the_simulation_1.on("tick", function() {
 
 
 // ---------------------------------------------------------------------------
-// radius 5 nodes
+// radius r=5 nodes
 var w_2 = 960;
 var h_2 = 300;
 
